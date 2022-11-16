c745ad6c03cb5b3a247ae446cc0578934969ddf5
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-11-16 16:14:26 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css";
+import classes from "./css/styles.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className={classes["app"]}>HOLLA MUNDO</div>;
 }
 
 export default App;
