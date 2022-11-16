ef051d8ae0ed72af28f7cb298fdfb190215df8ed
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-11-16 17:30:17 -0400

    teste de endpoint da api wger com axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import classes from "./css/styles.module.css";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className={classes["app"]}>HOLLA MUNDO</div>;
+  //TODO: Teste de endpoint
+
+  const [exercises, setExercises] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://wger.de/api/v2/exercise/")
+      .then(res => {
+        setExercises(res.data.results);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <div className={classes["app"]}>
+      <ul>
+        {exercises.map(ex => {
+          return <li key={ex.id}>{ex.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
