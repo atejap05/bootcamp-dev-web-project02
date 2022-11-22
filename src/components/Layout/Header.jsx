c71730346895c0a27c2d925e8fb2fc3107418b7d
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-11-22 16:44:17 -0400

    implementa favoritos

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import classes from "../../css/styles.module.css";
 import { Link } from "react-router-dom";
 import { HeartFilled } from "@ant-design/icons";
+import { useFavoritesContext } from "../../context/favoritesContext";
 
 const Header = () => {
+  const { favorites } = useFavoritesContext();
+
   return (
     <header className={classes.header}>
       <div>My AppBooks</div>
       <div className={classes["header__link"]}>
-        <span className={classes["header__link--count"]}>2</span>
+        <span className={classes["header__link--count"]}>
+          {favorites.length}
+        </span>
         <HeartFilled
           style={{ color: "#fca311" }}
           className={classes["header__link--heart"]}
