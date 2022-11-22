c43716d8807956bf7d3e3c1b5723e4cf4108554b
Joel Alves Pereira <atejap05@yahoo.com.br>
2022-11-22 17:05:49 -0400

    inicia estilizacao de card favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useFavoritesContext } from "../context/favoritesContext";
+import CardDetails from "../components/UI/CardDetails";
 
 const Favorites = () => {
-  return <Layout>Favorites</Layout>;
+  const { faddToFavorites, removeFromFavorites, favorites } =
+    useFavoritesContext();
+
+  console.log(favorites);
+  return (
+    <Layout>
+      {favorites.map(book => (
+        <CardDetails
+          key={book.id}
+          title={book.title}
+          image={book["image_url"]}
+          authors={book.authors}
+          genres={book.genres}
+          num_pages={book.num_pages}
+          rating={book.rating}
+          description={book.description}
+        />
+      ))}
+    </Layout>
+  );
 };
 
 export default Favorites;
